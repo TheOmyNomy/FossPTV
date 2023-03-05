87c89ec635f1ffb9d3b7c62d1982cb0c8dbe97f1
TheOmyNomy <theomynomy@nomy.sh>
2023-03-05 17:34:46 +1100

    Add `Developer ID` and `Developer Key` settings to the settings fragment

--- app/src/main/java/sh/nomy/fossptv/fragments/SettingsFragment.kt
+++ app/src/main/java/sh/nomy/fossptv/fragments/SettingsFragment.kt
@@ -1,19 +1,11 @@
 package sh.nomy.fossptv.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import sh.nomy.fossptv.R
 
-class SettingsFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
     }
 }
