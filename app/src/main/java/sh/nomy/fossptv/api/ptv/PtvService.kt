8265148ea34d675532b071c2711e1198a02de7a1
TheOmyNomy <theomynomy@nomy.sh>
2023-07-21 01:00:21 +1000

    Add parameters to `/search` endpoint

--- app/src/main/java/sh/nomy/fossptv/api/ptv/PtvService.kt
+++ app/src/main/java/sh/nomy/fossptv/api/ptv/PtvService.kt
@@ -3,6 +3,7 @@ package sh.nomy.fossptv.api.ptv
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 import sh.nomy.fossptv.api.ptv.models.RouteTypesResponse
 import sh.nomy.fossptv.api.ptv.models.SearchResponse
 
@@ -11,5 +12,15 @@ interface PtvService {
     fun getRouteTypes(): Call<RouteTypesResponse>
 
     @GET("search/{term}")
-    fun getSearch(@Path("term") term: String): Call<SearchResponse>
+    fun getSearch(
+        @Path("term") term: String,
+        @Query("route_types") routeTypes: List<Int>? = null,
+        @Query("latitude") latitude: Float? = null,
+        @Query("longitude") longitude: Float? = null,
+        @Query("max_distance") maxDistance: Float? = null,
+        @Query("include_outlets") includeOutlets: Boolean? = null,
+        @Query("match_stop_by_suburb") matchStopBySuburb: Boolean? = null,
+        @Query("match_route_by_suburb") matchRouteBySuburb: Boolean? = null,
+        @Query("match_stop_by_gtfs_stop_id") matchStopByGtfsStopId: Boolean? = null
+    ): Call<SearchResponse>
 }
